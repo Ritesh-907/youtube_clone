03996f1bdb073a016690dc7af8b046b855966119
Ritesh-907 <chaudharyritesh.7777@gmail.com>
2026-04-11 23:51:39 +0530

    all set up done

--- src/app.js
+++ src/app.js
@@ -9,8 +9,16 @@ app.use(cors({
     Credentials: true
 }))
 
-app.use(express.json(limit="16kb"))
+app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes
+import userRoutes from "./routes/user.routes.js";
+
+
+//routes decleration
+app.use("/api/v1/users",userRoutes)
 export { app }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from 'cloudinary'
+import { response } from 'express';
+import fs from 'fs'
+
+
+const uploadToCloudinary = async (filePath) => {
+    try {
+        if (!filePath) return null;
+        const response = await cloudinary.uploader.upload(filePath, { folder: "youflix", resource_type: "auto" });
+        console.log("File is upload on Cloudinary",response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filePath);
+        console.error("Error uploading file to Cloudinary", error);
+        throw error;
+    }
+}
+
+export default uploadToCloudinary;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
